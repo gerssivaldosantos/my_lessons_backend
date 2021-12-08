917f4c50a277f23857c2509412063fb6632677fb
gerssivaldosantos <canalandows@gmail.com>
2021-12-08 07:33:53 -0300

    route remove created

--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -9,7 +9,7 @@ const faker = require('faker');
 
 router.get('/api/lessons', async (req, res)=>{
     const lessons = await Lesson.find();
-    res.json(lessons);
+    res.json({lessons});
     
 });
 
@@ -22,6 +22,13 @@ router.get('/api/lessons/create', async (req, res)=>{
         })
     }
     res.json({message: "5 lessons created"})
+})
+
+router.get('/api/lessons/delete-all', async (req, res)=>{
+    await Lesson.remove(
+        {}
+    )
+    res.json('All data removed');
 })
 
 module.exports = router;
