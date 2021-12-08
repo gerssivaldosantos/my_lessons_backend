d9b3b8b6ab2973652c0a76bd6371cca1e9aac2f1
gerssivaldosantos <canalandows@gmail.com>
2021-12-07 23:14:56 -0300

    create example route and get data route createds

--- src/models/lessons.js
+++ src/models/lessons.js
@@ -3,7 +3,7 @@ const {Schema, model } = require('mongoose');
 const lessonSchema = new Schema ({
     Name: String,
     Description: String,
-    icon: String
+    Icon: String
 });
 
 module.exports = model('Lesson', lessonSchema);
--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -2,11 +2,25 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/api/lessons', (req, res)=>{
-    res.json('Lesson list');
+const Lesson = require('../models/lessons')
+
+const faker = require('faker');
+
+
+router.get('/api/lessons', async (req, res)=>{
+    const lessons = await Lesson.find();
+    res.json(lessons);
+    
 });
 
-router.get('/api/create', (req, res)=>{
+router.get('/api/lessons/create', async (req, res)=>{
+    for (let i = 0; i < 5; i++){
+        await Lesson.create({
+            Name: faker.name.firstName(),
+            Description: faker.vehicle.vehicle(),
+            Icon: faker.image.avatar()
+        })
+    }
     res.json({message: "5 lessons created"})
 })
 
