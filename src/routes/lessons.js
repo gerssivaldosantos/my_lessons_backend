ba416cc2b6d57a5a3553ae29847def5d915e271f
gerssivaldosantos <canalandows@gmail.com>
2021-12-09 01:04:07 -0300

    modify to create just one data in create route

--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -7,24 +7,24 @@ const Lesson = require('../models/lessons')
 const faker = require('faker');
 
 
-router.get('/api/lessons', async (req, res)=>{
+router.get('/api/lessons', async (req, res) => {
     const lessons = await Lesson.find();
-    res.json({lessons});
-    
+    res.json({ lessons });
+
 });
 
-router.get('/api/lessons/create', async (req, res)=>{
-  
-        await Lesson.create({
-            Name: faker.name.firstName(),
-            Description: faker.vehicle.vehicle(),
-            Icon: faker.image.avatar()
-        })
-    
-    res.json({message: "5 lessons created"})
+router.get('/api/lessons/create', async (req, res) => {
+
+    await Lesson.create({
+        Name: faker.name.firstName(),
+        Description: faker.vehicle.vehicle(),
+        Icon: faker.image.avatar()
+    })
+
+    res.json({ message: "lessons created" })
 })
 
-router.get('/api/lessons/delete-all', async (req, res)=>{
+router.get('/api/lessons/delete-all', async (req, res) => {
     await Lesson.remove(
         {}
     )
